5332d12e15790187c5a4efdbb8f219c128baf3f1
shidao <shidao@gmail.com>
2014-09-22 14:26:52 +0800

    --all

--- src/com/coolweather/app/db/CoolWeatherDB.java
+++ src/com/coolweather/app/db/CoolWeatherDB.java
@@ -155,7 +155,7 @@ public class CoolWeatherDB {
 
 	public List<County> loadCounties(int cityId) {
 		List<County> list = new ArrayList<County>();
-		Cursor cursor = db.query("County", null, "cityId = ?",
+		Cursor cursor = db.query("County", null, "city_id = ?",
 				new String[] { String.valueOf(cityId) }, null, null, null);
 
 		if (cursor.moveToFirst()) {
